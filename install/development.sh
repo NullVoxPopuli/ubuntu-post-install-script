dd0a135ff57a8581283d922804e8bad4e33ade39
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-04-23 16:12:52 -0400

    Create development.sh

---
+++ install/development.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Installs Development Tools
+# - RVM
+# - Ruby
+# - Atom Editor
+function install_development{
+
+  ######
+  # Ruby
+  # curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+}
