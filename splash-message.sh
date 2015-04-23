45ec91f13f9c434e449782a18d376a94bf948f06
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-04-23 16:05:15 -0400

    Create splash-message.sh

---
+++ splash-message.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# Prints summary and basic help.
+
+echo "\n
+echo "-------------------------------------------------\n"
+echo_with_color $Yellow "Ubuntu Post Install Script\n"
+echo_with_color $Cyan "  By: NullVoxPopuli / Preston Sego\n"
+echo "-------------------------------------------------\n"
+
+echo_with_color $Red "No help yet! :-("
