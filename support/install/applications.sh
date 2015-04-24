884d7aa543e00d822e491a4cf8412bda9d5f409a
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-04-24 01:04:48 -0400

    rename / reorganize

---
+++ support/install/applications.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Runs all scripts under applications
+
+source $(dirname $0)/applications/ruby.sh
+
+
+install_ruby
--- support/install/packages.sh
+++ support/install/packages.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-#
+
+source $(dirname $0)/packages/material-theme.sh
+
+
 # Installs more general system-ish packages
-function install_general{
+function install_packages{
+  ###############
+  # General Tools
+  add_package "git vim curl"
+
   #######
   # Gnome
   add_package "gnome"
 
-  ###############
-  # General Tools
-  add_package "git vim curl"
+  ########
+  # Others
+  setup_material_theme
 }
